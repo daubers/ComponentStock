61ec5024070dac80fad19081f0d86a50ab772835
Matthew Daubney <matt@daubers.co.uk>
2013-08-03 18:26:58 +0100

    Added some Jenkins stuff

--- ComponentStock/settings.py
+++ ComponentStock/settings.py
@@ -126,6 +126,10 @@ INSTALLED_APPS = (
     'Components',
 )
 
+PROJECT_APPS = (
+    'Stock',
+    'Components',
+)
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.
