28bc8f53d53177285c81f7f0e55965f56bbc65ff
Matthew Daubney <matt@daubers.co.uk>
2013-08-23 21:22:10 +0100

    Datatables now working and ajaxy stuff for getting components inserted. Needs refining.

--- StaticFiles/JS/viewComponents.js
+++ StaticFiles/JS/viewComponents.js
@@ -10,5 +10,15 @@ function createComponentsTable(){
     /*
         Gets the components and places them in the relevant table
      */
-
+    $(function() {$('#componentsTable').dataTable( {
+                        "bJQueryUI": true,
+                        "sAjaxSource": "/component/get/yes/",
+                        "aoColumns": [
+                            { "sTitle": "Name",   "mData": "name" },
+                            { "sTitle": "Datasheet",  "mData": "datasheet_uri" },
+                            { "sTitle": "Maximum Quantity", "mData": "max_quantity"},
+                            { "sTitle": "Minimum Quantity", "mData": "min_quantity"}
+                            ]
+                        } );
+    });
 }
